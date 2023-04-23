13b42fb9ceca9e090ab3f3c2c054c7c8b1e29b10
Daniel Mirochna <DanielM6@protonmail.com>
2023-04-23 12:26:00 +0200

    Solved

--- count_elements.rb
+++ count_elements.rb
@@ -1,3 +1,11 @@
 class CountElements
+  def initialize(array)
+    @array = array
+  end
 
+  def perform
+    counted_hash = @array.uniq.map {|e| [e,0] }.to_h
+    @array.uniq.each { |elem| counted_hash[elem] = @array.count(elem) }
+    counted_hash
+  end
 end
--- validate_password.rb
+++ validate_password.rb
@@ -1,3 +1,44 @@
 class ValidatePassword
 
+  def initialize(password)
+    @password = password
+    @password_array = password.split('')
+  end
+
+  def perform
+    check_length &&
+    check_downcase &&
+    check_uppercase &&
+    check_digits &&
+    check_special_chars &&
+    check_max_chars_amount
+  end
+
+  def check_length
+    @password.length.between?(6,24)
+  end
+
+  def check_uppercase
+    @password.downcase != @password
+  end
+
+  def check_downcase
+    @password.upcase != @password
+  end
+
+  def check_digits
+    !@password[/\d/].nil?
+  end
+
+  def check_special_chars
+    @password_array.each do |char|
+      return true if %w{! @ # $ % & * + = : ; ? < >}.include?(char)
+    end
+    false
+  end
+
+  def check_max_chars_amount
+    @password_array.each { |char| return false if @password_array.count(char) > 2 }
+    true
+  end
 end
